efc213b4cdc93ead55f809ed02c062f8dd9898d4
serafino <abraham.serafino@gmail.com>
2017-04-07 06:36:48 -0500

    Fix key bindings

--- src/index.js
+++ src/index.js
@@ -11,19 +11,20 @@ window.addEventListener('keyup', event => {
 
   const KEY_ACTION_MAP = {
     8: 'CLEAR',
-    10: 'MULTIPLY',
     13: 'TOTAL',
     46: 'CLEAR_EVERYTHING',
-    107: 'ADD',
-    109: 'SUBTRACT',
-    111: 'DIVIDE',
-    187: 'TOTAL',
-    191: 'DIVIDE'
+    '+': 'ADD',
+    '-': 'SUBTRACT',
+    '*': 'MULTIPLY',
+    '/': 'DIVIDE',
+    '=': 'TOTAL'
   };
 
   if (isNumeric(key)) {
     store.dispatch({ type: 'INPUT_NUMBER', input: key });
-  } else if (KEY_ACTION_MAP[keyCode]) {
+  } else if (KEY_ACTION_MAP[key]) {
+    store.dispatch({ type: KEY_ACTION_MAP[key] });
+  }else if (KEY_ACTION_MAP[keyCode]) {
     store.dispatch({ type: KEY_ACTION_MAP[keyCode] });
   }
 });
